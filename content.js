37238e4179964d34bf23494b54dcec27ff6da191
Joel Howard <joelhoward2@gmail.com>
2023-03-25 22:11:13 -0400

    Set predifined text to desired string

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 console.log('contents.js first line executed')
 
-const predefinedText = 'Your predefined text here';
+const predefinedText = 'Please use the Socratic method to teach me *all* of the following content; please do *NOT* ask more than one question and please do *NOT* answer your own questions:\n\n';
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'pastePredefinedText') {
