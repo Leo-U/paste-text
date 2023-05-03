5e1d8d15c14a95a39b73f7b82a850037689d293e
Joel Howard <joelhoward2@gmail.com>
2023-05-02 20:22:24 -0400

    Modify prompt

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 console.log('contents.js first line executed')
 
-const predefinedText = 'Please use the Socratic method to teach me *all* of the following content; please do *NOT* ask more than one question and please do *NOT* answer your own questions:\n\n';
+const predefinedText = 'Please use the Socratic method to teach me *all* of the following content; please do *NOT* ask more than one question PER REPLY and please do *NOT* answer your own questions:\n\n';
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'pastePredefinedText') {
